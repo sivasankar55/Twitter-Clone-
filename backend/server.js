2ff775352abee3b8009c3a5e580008b1fdca54cd
sivasankar55 <sivasankar8875@gmil.com>
2025-02-17 00:36:13 +0530

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("server is urning")
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT  || 5000;
+
+app.use("/api/auth",authRoutes );
+
+console.log(process.env.MONGO_URI);
+
+app.listen( PORT, () => {
+    connectMongoDB();
+    console.log(`server is running on port ${PORT}`);
+})
