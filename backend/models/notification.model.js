d4c4b022c4f22978784d2952cfca3cb39eb38a16
sivasankar55 <sivasankar8875@gmil.com>
2025-02-19 11:30:30 +0530

    usercontroller and user router

---
+++ backend/models/notification.model.js
@@ -0,0 +1,28 @@
+import mongoose, { model } from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+    from:{
+        type: mongoose.Schema.ObjectId,
+        ref:"User",
+        required:true
+    },
+    to:{
+        type: mongoose.Schema.ObjectId,
+        ref:"User",
+        required:true
+    },
+    type:{
+        type:String,
+        required: true,
+        enum:["follow","like"]
+    },
+    read:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true});
+
+
+const Notification = mongoose.model("Notification",notificationSchema);
+
+export default Notification;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT  || 5000;
@@ -13,7 +22,9 @@ app.use(express.json()); // to parse req.body // middleware
 app.use(express.urlencoded({extended: true})); // to parse form data (urlencoded)
 
 app.use(cookieParser());
+
 app.use("/api/auth",authRoutes );
+app.use("/api/users", userRoutes);
 
 console.log(process.env.MONGO_URI);
 
