413b36d081de1c633433628f44d4220ff044a490
sivasankar55 <sivasankar8875@gmil.com>
2025-02-21 20:16:58 +0530

    update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
